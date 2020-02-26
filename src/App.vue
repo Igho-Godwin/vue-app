a5a1ee9be70cd11ed4101ef1a3b31ac83c7e55db
Femi Oye <femi@reach.africa>
2020-02-26 20:40:17 +0100

    fixes for routing

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to REACH for Business"/>
-    <nav>
-      <router-link to="./components/home/Sign">Sign</router-link>
-      <router-link to="./components/nav-element/Landing">Landing</router-link>
-      <router-link to="./components/nav-element/Dashboard">Dashboard</router-link>
-    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  routes: [
-    { path: './components/home/Sign', component: Sign},
-    { path: './components/nav-element/Landing', component: Landing },
-    { path: './components/nav-element/Dashboard', component: Dashboard }
-  ]
-})
-
-new Vue({
-  router
-}).$mount('#app')
-
+import HelloWorld from './components/HelloWorld.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Sign,
-    Landing,
-    Dashboard
+    HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { 
+      path: '/sign-in',
+      component: () => import("@/components/home/Sign")
+    },
+    { 
+      path: '/',
+      component: () => import("@/components/nav-element/Landing")
+    },
+    { 
+      path: '/dashboard',
+      component: () => import("@/components/nav-element/Dashboard")
+    }
+  ]
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
