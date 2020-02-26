62dafc111cd77ba4e7537b5a97539b38c08fbabb
jide_omotola <jide@reach.africa>
2020-02-26 14:37:02 +0100

    App router added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,40 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to REACH for Business"/>
+    <nav>
+      <router-link to="./components/home/Sign">Sign</router-link>
+      <router-link to="./components/nav-element/Landing">Landing</router-link>
+      <router-link to="./components/nav-element/Dashboard">Dashboard</router-link>
+    </nav>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: './components/home/Sign', component: Sign},
+    { path: './components/nav-element/Landing', component: Landing },
+    { path: './components/nav-element/Dashboard', component: Dashboard }
+  ]
+})
+
+new Vue({
+  router
+}).$mount('#app')
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Sign,
+    Landing,
+    Dashboard
   }
 }
 </script>
