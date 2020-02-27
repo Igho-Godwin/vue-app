0e98bd422a5258b9e8c9495ae3bbbbd2a1de9b59
jide_omotola <jide@reach.africa>
2020-02-27 12:29:42 +0100

    default and blank layout pages

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default-layout">
+    <main class="app-main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+    
+  }  
+}
+</script>
