33eb7ed81c390d4d0471d499ab74d016c8e83465
jide_omotola <jide@reach.africa>
2020-02-28 16:34:53 +0100

    sign-up route added

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,7 +1,7 @@
 
 export const routes = [{
   //authentications
-    path: '/sign-in',
+    path: '/',
     component: () => import("@/components/authentications/SignIn"),
     meta: { layout: "auth" }
   },
@@ -11,4 +11,9 @@ export const routes = [{
     meta: {layout: 'dashboard'},
     component: () => import("@/components/pages/Dashboard")
   },
+  {
+    path: '/sign-up',
+    component: () => import("@/components/authentications/SignUp"),
+    meta: {layout: 'sign-up'}
+  }
 ];
