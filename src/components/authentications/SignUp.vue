8e29ac58a6cc8f548c87f2c7ee0b6b4eda995b6c
Femi Oye <femi@reach.africa>
2020-03-02 16:38:38 +0100

    Authentication styling and refactor

--- src/components/authentications/SignUp.vue
+++ src/components/authentications/SignUp.vue
@@ -1,5 +1,37 @@
 <template>
-  <section class="sign-up">
+  <Authentication title="Create your Account" authBG="bg-signup.jpg">
+    <p slot="explainer">Already have an account? <router-link to="/sign-in">Sign In</router-link></p>
+    <form>
+      <div class="form-group">
+        <label for="businessName" class="caption">Business Name</label>
+        <input type="text" class="form-control" id="businessName" required>
+      </div>
+      <div class="form-group">
+        <label for="workEmail" class="caption">Your Work Email Address</label>
+        <input type="text" class="form-control" id="workEmail" required>
+      </div>
+      <div class="form-group">
+        <label for="businessType" class="caption">Business Name</label>
+        <select class="form-control text-regular" id="businessType" required>
+          <option selected>Choose...</option>
+          <option>Faith &amp; Charity</option>
+          <option>Transportation</option>
+          <option>Restaurants</option>
+          <option>Entertainment</option>
+          <option>Grocery</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="password" class="caption">Create Password</label>
+        <input type="text" class="form-control" id="password" required>
+      </div>
+      
+      <div class="form-group">
+        <button type="submit" class="btn-blue btn btn-block">Create My Account</button>
+      </div>
+    </form>
+  </Authentication>
+  <!-- <section class="sign-up">
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-md-4 sign-in__form">
@@ -10,7 +42,7 @@
           </div>
           <form>
             <div class="form-group text-small text-start">
-               <label for="businessName">BUSINESS NAME</label>
+               <label for="businessName" class="caption">BUSINESS NAME</label>
             <input type="text" class="form-control text-regular" id="businessName" required>
             </div>
             <div class="form-group text-small text-start">
@@ -39,48 +71,15 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 </template>
 
 <script>
+  import Authentication from "@/components/Authentication";
   export default {
-    name: 'sign-up'
+    name: "Sign-Up",
+    components: {
+      Authentication
+    }
   }
 </script>
-
-<style lang="scss" scoped>
-  @import "@/scss/vendors/themed-bootstrap.scss";
-  @import "@/scss/main.scss";
-
-  .text-center {
-    text-align: center;
-  }
-
-  .text-start {
-    text-align: start;
-    font-weight: $semi-bold;
-  }
-
-  .sign-up {
-    @include breakpoint-min (lg) {
-      &::before {
-        content: '';
-        display: block;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 30%;
-        height: 100%;
-        background: yellow;
-      }
-    }
-  }
-
-  .sign-in__form {
-
-    @include breakpoint-min (lg) {
-      margin-left: 35%;
-      padding: 3rem;
-    }
-  }
-</style>
