308f5162f64fa6d6dcf888fcc1adfa64a4956aaf
Femi Oye <femi@reach.africa>
2020-03-03 08:29:00 +0100

    added welcome lottie animation

--- src/pages/onboarding/Step1.vue
+++ src/pages/onboarding/Step1.vue
@@ -2,9 +2,9 @@
   <Onboarding progress="Step 1 of 3" title="Welcome!">
     <Card>
       <div slot="card-body">
-        <div class="card__gif py-5">Gif</div>
+        <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_ISbOsd.json" background="transparent" speed="1" loop autoplay ></lottie-player>
         <p class="text-center">Welcome to REACH for Business, you can post your deals, and see reviews from your customers. </p>
-        <button type="submit" class="btn btn-blue btn-block mt-5">Setup Company</button>
+        <button type="submit" class="btn btn-blue btn-block mt-5">Setup your Company</button>
       </div>
     </Card>
   </Onboarding>
@@ -19,6 +19,11 @@
     components: {
       Onboarding,
       Card
+    },
+    mounted() {
+      const scriptTag = document.createElement("script");
+      scriptTag.src = "https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js";
+      document.getElementsByTagName("head")[0].appendChild(scriptTag);
     }
   }
 </script>
