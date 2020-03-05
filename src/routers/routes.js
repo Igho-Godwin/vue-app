00e9ead96b619ff5584c71eaa8f9fd08f9340e03
jide_omotola <jide@reach.africa>
2020-03-05 13:50:43 +0100

    routes created for marketinsights, deals and reviews

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -68,5 +68,29 @@ export const routes = [
   {
     path: '/all-cards',
     component: () => import("@/pages/AllCards")
-  }
+  },
+  {
+    path: '/deals',
+    component: () => import("@/pages/Deals"),
+    meta: {
+      layout: 'dashboard',
+      headerTitle: 'Deals'
+    }
+  },
+  {
+    path: '/market-insights',
+    component: () => import("@/pages/MarketInsights"),
+    meta: {
+      layout: 'dashboard',
+      headerTitle: 'MarketInsights'
+    }
+  },
+  {
+    path: 'reviews',
+    component: () => import("@/pages/Reviews"),
+    meta: {
+      layout: 'dashboard',
+      headerTitle: 'Reviews'
+    }
+  },
 ];
