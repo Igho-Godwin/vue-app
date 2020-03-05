7ed3343b7f7a704937bc036bb6808183778c3f4f
Femi Oye <femi@reach.africa>
2020-03-05 09:50:41 +0100

    settings navigation

--- src/pages/settings/CheckoutSetting.vue
+++ src/pages/settings/CheckoutSetting.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="checkoutSetting">
-    <BankInformation />
+    <SettingsNav :activeNav="1" />
+    <div class="container-fluid py-3">
+      <div class="row">
+        <div class="col-lg-8 col-xl-6">
+          <BankInformation />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import BankInformation from "@/components/settings/BankInformation";
+  import SettingsNav from "@/components/settings/SettingsNav";
   export default {
     components: {
-      BankInformation
+      BankInformation,
+      SettingsNav
     },
     mounted() {
       this.$emit('headerTitle')
--- src/pages/settings/OrganisationSetting.vue
+++ src/pages/settings/OrganisationSetting.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="organisationSetting">
-    <div class="row">
-      <div class="col-lg-6">
-        <CompanyInformation />
-      </div>
-      <div class="col-lg-6">
-        <BusinessLogo />
+    <SettingsNav :activeNav="2" />
+    <div class="container-fluid py-3">
+      <div class="row">
+        <div class="col-lg-6">
+          <CompanyInformation />
+        </div>
+        <div class="col-lg-6">
+          <BusinessLogo />
+        </div>
       </div>
     </div>
   </div>
@@ -14,10 +17,12 @@
 <script>
   import CompanyInformation from "@/components/settings/CompanyInformation";
   import BusinessLogo from "@/components/settings/BusinessLogo";
+  import SettingsNav from "@/components/settings/SettingsNav";
   export default {
     components: {
       CompanyInformation,
-      BusinessLogo
+      BusinessLogo,
+      SettingsNav
     },
     mounted() {
       this.$emit('headerTitle')
