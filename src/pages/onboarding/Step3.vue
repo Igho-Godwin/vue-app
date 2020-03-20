1ca5cc8d0d62a02e21c01bd65016ad138c378eb6
jide_omotola <jide@reach.africa>
2020-03-20 22:17:24 +0100

    added spinner button class and js functions

--- src/pages/onboarding/Step3.vue
+++ src/pages/onboarding/Step3.vue
@@ -7,34 +7,18 @@
           results.
         </p>
         <form>
-          <my-upload
-            field="img"
-            @crop-success="cropSuccess"
-            @crop-upload-success="cropUploadSuccess"
-            @crop-upload-fail="cropUploadFail"
-            v-model="show"
-            :width="127"
-            :height="127"
-            url
-            :params="params"
-            :headers="headers"
-            img-format="png"
-            lang-type="en"
-            :value.sync="show"
-          ></my-upload>
+          <my-upload field="img" @crop-success="cropSuccess" @crop-upload-success="cropUploadSuccess"
+            @crop-upload-fail="cropUploadFail" v-model="show" :width="127" :height="127" url :params="params"
+            :headers="headers" img-format="png" lang-type="en" :value.sync="show"></my-upload>
           <label for="businessLogo" class="logo-showcase" @click="toggleShow">
             <img class="fas fa-camera preview text-center" id="businessLogo" :src="imgDataUrl" />
           </label>
           <div class="form-group" @click="toggleShow">
-            <input
-              id="businessLogo"
-              class="form-control"
-              placeholder="Choose file"
-              :src="imgDataUrl"
-            />
+            <input id="businessLogo" class="form-control" placeholder="Choose file" :src="imgDataUrl" />
           </div>
-          
-          <button type="button" @click='submit()' class="btn btn-blue btn-block mt-5">Save &amp; Continue</button>
+
+          <button type="button" @click='submit();handleClick()' class="btn btn-blue btn-block mt-5">Save &amp;
+            Continue&nbsp;<span id="spin" class="fa fa-spin"></span></button>
         </form>
       </div>
     </Card>
@@ -88,35 +72,35 @@
         this.show = !this.show;
       },
       submit() {
-      let access_token = this.$store.getters.user.access_token;
-      let payload = this.$store.getters.initial_setup.data;
-      payload.business_id = this.$store.getters.user.id;
-      payload.company_logo = this.imgDataUrl;
-      fetch(this.getGlobalUrl + "checkout/onboardingSetup?access_token="+access_token, {
-        method: "put", // or 'PUT'
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(payload)
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data) {
-            if (data.status == "success") {
-              data.data.access_token = access_token;
-              this.$store.dispatch("ADD_USER", data.data);
-              this.$store.dispatch('CLEAR_INITIAL_SETUP_DATA');
-               window.open("/dashboard", "_self");
-            } else {
-              this.$swal(data.message);
+        let access_token = this.$store.getters.user.access_token;
+        let payload = this.$store.getters.initial_setup.data;
+        payload.business_id = this.$store.getters.user.id;
+        payload.company_logo = this.imgDataUrl;
+        fetch(this.getGlobalUrl + "checkout/onboardingSetup?access_token=" + access_token, {
+            method: "put", // or 'PUT'
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+          })
+          .then(response => response.json())
+          .then(data => {
+            if (data) {
+              if (data.status == "success") {
+                data.data.access_token = access_token;
+                this.$store.dispatch("ADD_USER", data.data);
+                this.$store.dispatch('CLEAR_INITIAL_SETUP_DATA');
+                window.open("/dashboard", "_self");
+              } else {
+                this.$swal(data.message);
+              }
             }
-          }
-          console.log("Success:", data);
-        })
-        .catch(error => {
-          console.log("Error:", error);
-        });
+            console.log("Success:", data);
+          })
+          .catch(error => {
+            console.log("Error:", error);
+          });
 
         return false;
       },
@@ -152,35 +136,42 @@
         console.log('-------- upload fail --------');
         console.log(status);
         console.log('field: ' + field);
-      }
+      },
+      handleClick() {
+        var element = document.getElementById("spin");
+        element.classList.add("fa-spinner");
+        setTimeout(() => {
+          element.classList.remove("fa-spinner");
+        }, 1000)
+      },
     }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "@/scss/abstracts/_variables.scss";
+  @import "@/scss/abstracts/_variables.scss";
 
-.logo-showcase {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 2rem auto;
-  max-width: 8rem;
-  width: 100%;
-  height: 8rem;
-  border: 1px dashed $smoke;
-  background-color: $snow;
-  font-size: 1.5rem;
-  color: $secondary-text;
-  cursor: pointer;
+  .logo-showcase {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2rem auto;
+    max-width: 8rem;
+    width: 100%;
+    height: 8rem;
+    border: 1px dashed $smoke;
+    background-color: $snow;
+    font-size: 1.5rem;
+    color: $secondary-text;
+    cursor: pointer;
 
-  &:hover {
-    border-color: $dark-smoke;
+    &:hover {
+      border-color: $dark-smoke;
+    }
   }
-}
 
-img.preview {
-  max-width: 8rem;
-  width: 100%;
-}
+  img.preview {
+    max-width: 8rem;
+    width: 100%;
+  }
 </style>
